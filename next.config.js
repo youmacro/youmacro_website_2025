4a54ea30c0a7092a92408ca5248f00eb715caeb0
nodegraph <nodegraph@gmail.com>
2025-01-25 14:35:37 -0800

    test

--- next.config.js
+++ next.config.js
@@ -21,20 +21,4 @@ module.exports = withBundleAnalyzer({
     // !! WARN !!  Note this is needed to use ms-store-badge.
     ignoreBuildErrors: true,
   },
-
-  // This is to enable cors, so that the react-share buttons show up.
-  async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
 });
