9e58807924cd61feafb7f9976de64a2f6352b316
nodegraph <nodegraph@gmail.com>
2025-10-19 12:34:09 -0700

    adding gtags to site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+
 import '../styles/global.css';
 
-import type { AppProps } from 'next/app';
-
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    {/* Google tag (gtag.js) */}
+    <Script
+      strategy="afterInteractive"
+      src="https://www.googletagmanager.com/gtag/js?id=G-XDS0CF3W2R"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-XDS0CF3W2R');
+      `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
